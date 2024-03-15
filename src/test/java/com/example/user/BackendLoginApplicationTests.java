67dfd4990c6add1a3137ec82f337f6eecdc216b3
amitpr1237 <amitpr1237@gmail.com>
2024-03-15 15:33:04 +0530

    third commit

--- src/test/java/com/example/user/BackendLoginApplicationTests.java
+++ src/test/java/com/example/user/BackendLoginApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.authentication.BackendLoginApplication;
+
 @SpringBootTest(classes = BackendLoginApplication.class)
 class BackendLoginApplicationTests {
 
